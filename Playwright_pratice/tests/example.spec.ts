a02ff87758dbdb2b16135a667e8d29f6a6e91dde
jeevapranesh005 <jeevapranesh.vrp@gmail.com>
2026-06-24 17:25:54 +0530

    playwrite

---
+++ Playwright_pratice/tests/example.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+test('login Test',async({page})=>{
+
+  await page.goto(process.env.BASE_URL!);
+
+  await page.fill('#username',process.env.UNAME!);
+  await page.fill('#password',process.env.PASS!);
+
+  await page.click('.radius');
+
+  let msg:any = await page.locator(".subheader").textContent()
+  console.log(msg)
+});
---
+++ Playwright_pratice/tests/tutorialNinja.spec.ts
@@ -0,0 +1,17 @@
+import {test,expect} from "@playwright/test"
+
+test.beforeEach(async({page})=>{
+    await page.goto(process.env.BASE_URL!);
+})
+
+
+test('login',async({page})=>{
+
+   
+    await page.click("//span[text()='My Account']")
+    await page.click("//ul//a[text()='Login']")
+    await page.fill("#input-email",process.env.UNAME!)
+    await page.fill("#input-password",process.env.PASS!)
+    await page.click("//input[@value='Login']")
+    
+})
