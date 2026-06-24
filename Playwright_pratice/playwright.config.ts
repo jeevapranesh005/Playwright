8cf4503f46db29927f5790bb2bb77ed5fc651cf0
jeevapranesh005 <jeevapranesh.vrp@gmail.com>
2026-06-24 22:22:39 +0530

    tutorialNinja

--- Playwright_pratice/playwright.config.ts
+++ Playwright_pratice/playwright.config.ts
@@ -22,10 +22,12 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html'],['allure-playwright']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
+  
  use: {
-    trace: 'on-first-retry',
+    trace: 'on',
     headless: true,
     screenshot: 'only-on-failure',
     video:'retain-on-failure',
